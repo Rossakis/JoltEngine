41bdf3ab956f14329bb66f1b6350dcac7e4ba909
Georgios Tsolakidis <rossakis@hotmail.com>
2025-02-13 14:41:55 +0200

    Added events and ui for easier management of save files

--- Nez.ImGui/Inspectors/SceneGraphWindow.cs
+++ Nez.ImGui/Inspectors/SceneGraphWindow.cs
@@ -7,41 +7,126 @@ using Nez.ImGuiTools.SceneGraphPanes;
 using Num = System.Numerics;
 
 
-namespace Nez.ImGuiTools
+namespace Nez.ImGuiTools;
+
+public class SceneGraphWindow
 {
-	class SceneGraphWindow
+	private PostProcessorsPane _postProcessorsPane = new();
+	private RenderersPane _renderersPane = new();
+	private EntityPane _entityPane = new();
+
+	#region Event Handlers
+
+	public event Action OnSaveEntityChanges;
+	public event Action OnSaveSceneChanges;
+	public event Action OnSaveAllChanges;
+	public event Action OnCancelChanges;
+
+	private void InvokeSaveEntityChanges()
 	{
-		PostProcessorsPane _postProcessorsPane = new PostProcessorsPane();
-		RenderersPane _renderersPane = new RenderersPane();
-		EntityPane _entityPane = new EntityPane();
+		OnSaveEntityChanges?.Invoke();
+	}
 
-		public void OnSceneChanged()
+	private void InvokeSaveSceneChanges()
+	{
+		OnSaveSceneChanges?.Invoke();
+	}
+
+	private void InvokeSaveAllChanges()
+	{
+		OnSaveAllChanges?.Invoke();
+	}
+
+	private void InvokeCancelChanges()
+	{
+		OnCancelChanges?.Invoke();
+	}
+
+	#endregion
+
+	public void OnSceneChanged()
+	{
+		_postProcessorsPane.OnSceneChanged();
+		_renderersPane.OnSceneChanged();
+	}
+
+	public void Show(ref bool isOpen)
+	{
+		if (Core.Scene == null || !isOpen)
+			return;
+
+		ImGui.SetNextWindowPos(new Num.Vector2(0, 25), ImGuiCond.FirstUseEver);
+		ImGui.SetNextWindowSize(new Num.Vector2(300, Screen.Height / 2), ImGuiCond.FirstUseEver);
+
+		if (ImGui.Begin("Scene Graph", ref isOpen))
 		{
-			_postProcessorsPane.OnSceneChanged();
-			_renderersPane.OnSceneChanged();
+			if (ImGui.CollapsingHeader("Post Processors"))
+				_postProcessorsPane.Draw();
+
+			if (ImGui.CollapsingHeader("Renderers"))
+				_renderersPane.Draw();
+
+			if (ImGui.CollapsingHeader("Entities (double-click label to inspect)", ImGuiTreeNodeFlags.DefaultOpen))
+				_entityPane.Draw();
+
+			NezImGui.MediumVerticalSpace();
+			if (NezImGui.CenteredButton("Save Changes", 0.7f))
+				ImGui.OpenPopup("save-changes");
+
+			NezImGui.MediumVerticalSpace();
+			if (NezImGui.CenteredButton("Cancel Changes", 0.7f))
+				ImGui.OpenPopup("cancel-changes");
+
+			DrawSaveChangesPopup();
+			DrawCancelChangesPopup();
+
+			ImGui.End();
 		}
+	}
 
-		public void Show(ref bool isOpen)
+	private void DrawSaveChangesPopup()
+	{
+		if (ImGui.BeginPopup("save-changes"))
 		{
-			if (Core.Scene == null || !isOpen)
-				return;
+			ImGui.Text("Select SAVE mode");
 
-			ImGui.SetNextWindowPos(new Num.Vector2(0, 25), ImGuiCond.FirstUseEver);
-			ImGui.SetNextWindowSize(new Num.Vector2(300, Screen.Height / 2), ImGuiCond.FirstUseEver);
-
-			if (ImGui.Begin("Scene Graph", ref isOpen))
+			NezImGui.SmallVerticalSpace();
+			if (NezImGui.CenteredButton("Save ENTITY", 1f))
 			{
-				if (ImGui.CollapsingHeader("Post Processors"))
-					_postProcessorsPane.Draw();
-
-				if (ImGui.CollapsingHeader("Renderers"))
-					_renderersPane.Draw();
-
-				if (ImGui.CollapsingHeader("Entities (double-click label to inspect)", ImGuiTreeNodeFlags.DefaultOpen))
-					_entityPane.Draw();
-
-				ImGui.End();
+				InvokeSaveEntityChanges();
+				ImGui.CloseCurrentPopup();
 			}
+
+			NezImGui.SmallVerticalSpace();
+			if (NezImGui.CenteredButton("Save SCENE", 1f))
+			{
+				InvokeSaveSceneChanges();
+				ImGui.CloseCurrentPopup();
+			}
+
+			NezImGui.SmallVerticalSpace();
+			if (NezImGui.CenteredButton("Save ALL", 1f))
+			{
+				InvokeSaveAllChanges();
+				ImGui.CloseCurrentPopup();
+			}
+		}
+	}
+
+	private void DrawCancelChangesPopup()
+	{
+		if (ImGui.BeginPopup("cancel-changes"))
+		{
+			ImGui.Text("Are you sure you want to CANCEL ALL changes?");
+
+			if (ImGui.Button("Yes"))
+			{
+				InvokeCancelChanges();
+				ImGui.CloseCurrentPopup();
+			}
+
+			if (ImGui.Button("No"))
+				ImGui.CloseCurrentPopup();
 		}
 	}
 }
